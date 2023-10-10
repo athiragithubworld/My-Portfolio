a9a4510bf7a9e1db80f6b6559fea7306a02bfdd9
athiragithubworld <athirahrms@gmail.com>
2023-10-10 15:32:36 +0530

    Index file and App file committed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import logo from "./logo.svg";
+import "./App.css";
+import Contact from "./Components/Contact/Contact";
+import Experience from "./Components/Experience/Experience";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import Nav from "./Components/Nav/Nav";
+import Portfolio from "./Components/Portfolio/Portfolio";
+import Services from "./Components/Services/Services";
+import Testimonials from "./Components/Testimonials/Testimonials";
+import About from "./Components/about/About";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      {/* <Services /> */}
+      <Portfolio />
+      {/* <Testimonials /> */}
+      <Contact />
+      <Footer />
+    </>
   );
 }
 

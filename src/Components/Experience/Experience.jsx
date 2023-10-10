0cecc7fdb65cf90c67d6c47ff1c43ffa2c365468
athiragithubworld <athirahrms@gmail.com>
2023-10-10 15:36:15 +0530

    Experience committed

---
+++ src/Components/Experience/Experience.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import "./Experience.css";
+import { BsFillPatchCheckFill } from "react-icons/bs";
+
+const Experience = () => {
+  return (
+    <section id="experience">
+      <h5>What Skills I have </h5>
+      <h2>Technical Skills</h2>
+      <div className="container experience_container">
+        <div className="experience_frontend">
+          <h3>Skills</h3>
+          <div className="experience_content">
+            <article className="experience_details">
+              <BsFillPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>HTML</h4>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsFillPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>CSS</h4>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsFillPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>JavaScript</h4>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsFillPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>React</h4>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsFillPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>Bootstrap</h4>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsFillPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>Python</h4>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsFillPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>Next JS</h4>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsFillPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>Mongo DB</h4>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsFillPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>C# DotNet</h4>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsFillPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>SQL</h4>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsFillPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>GitHub</h4>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Experience;
